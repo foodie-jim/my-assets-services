967c2c251913bd57ba6634ec8b240ddc07dbf098
jim_kim_ <hjman.kim@gmail.com>
2021-08-15 01:07:57 +0900

    Test Update

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express'
 const app = express();
 
 app.get('/', (_req: Request, res: Response) => {
-    res.send('Hello World');
+    res.send('Hello World, keep me alive');
 });
 
 app.listen(process.env.PORT || 3000, () => {
