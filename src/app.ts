8cdf7eab526933d5c95f1dbed26b45b624cac593
jim_kim_ <hjman.kim@gmail.com>
2021-08-15 00:13:55 +0900

    Setup eslint

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,11 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express();
 
-app.get('/', (_req: Request, res: Response, _next: NextFunction) => {
-
+app.get('/', (_req: Request, res: Response) => {
     res.send('Hello World');
 });
 
 app.listen(80, () => {
-    console.log('Server is running');
+
 });
-// eslint 적용해서 아래 것 eslint 에러 나오게 해라
-function Spelling (work) 
-{
-console.log(1);
-}
